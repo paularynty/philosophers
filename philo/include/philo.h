7987a8a18ca6c75c2823e265b8abccb351a9001d
paularynty <paula.rynty@gmail.com>
2024-10-12 16:54:38 +0300

    slowly learning the concepts

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: prynty <paula.rynty@student.hive.fi>       +#+  +:+       +#+        */
+/*   By: prynty <prynty@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 14:38:32 by prynty            #+#    #+#             */
-/*   Updated: 2024/10/08 16:29:59 by prynty           ###   ########.fr       */
+/*   Updated: 2024/10/12 15:42:31 by prynty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <stdlib.h> //for malloc, free
 # include <stdio.h> //for printf
 # include <pthread.h> //for pthread functions
 # include <sys/time.h> //for gettimeofday
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: prynty <paula.rynty@student.hive.fi>       +#+  +:+       +#+        */
+/*   By: prynty <prynty@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 14:38:20 by prynty            #+#    #+#             */
-/*   Updated: 2024/10/08 16:27:22 by prynty           ###   ########.fr       */
+/*   Updated: 2024/10/12 16:47:59 by prynty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../include/philo.h"
 
 /*
 TODO:
@@ -19,47 +21,107 @@ TODO:
 5. Initiate the threads with said routine
 */
 
-#include "../include/philo.h"
+// void	init_struct(t_philo *philo)
+// {
+// 	philo->argc = argc;
+// 	philo->argv = argv;
+// 	philo->nbr = argv[1];
+// 	philo->die = argv[2];
+// 	philo->eat = argv[3];
+// 	philo->sleep = argv[4];
+// }
 
-void	init_struct(t_philo *philo)
+// static int	validate_args(t_philo *philo)
+// {
+// 	if (philo->argc != 6)
+// 	{
+// 		write(2, "Usage: ./philosophers number_of_philosophers time_to_die time_to_eat time_to_sleep number_of_times_each_philosopher_must_eat\n", 125);
+// 		return (1);
+// 	}
+// 	return (0);
+// }
+
+// //number of philosophers, time to die, time to eat, time to sleep, (number 
+// //of times each philosopher must eat)
+// int	main(int argc, char **argv)
+// {
+// 	t_philo	philo;
+// 	size_t	i;
+// 	pthread_mutex_t	mutex;
+
+// 	i = 0;
+// 	init_struct(&philo);
+// 	if (validate_args(&philo))
+// 		return (-1);
+// 	pthread_mutex_init(&mutex, NULL);
+// 	while (i <= philo.nbr)
+// 	{
+// 		pthread_create(pthread_t *restrict thread, const pthread_attr_t *restrict attr, void *(*start_routine)(void *), void *restrict arg);
+// 		i++;
+// 	}
+// 	pthread_mutex_destroy(&mutex);
+// 	return (0);
+// }
+
+pthread_mutex_t	mutex;
+
+
+void	routine(void) 
 {
-	philo->argc = argc;
-	philo->argv = argv;
-	philo->nbr = argv[1];
-	philo->die = argv[2];
-	philo->eat = argv[3];
-	philo->sleep = argv[4];
+	int i = 0;
+	while (i < 5)
+	{
+		i++;
+		pthread_mutex_lock(&mutex);
+		printf("Philosopher %d has taken a fork\n", i);
+		usleep(5000);
+		pthread_mutex_unlock(&mutex);
+		pthread_mutex_lock(&mutex);
+		printf("Philosopher %d is eating\n", i);
+		usleep(5000);
+		pthread_mutex_unlock(&mutex);
+		pthread_mutex_lock(&mutex);
+		printf("Philosopher %d is thinking\n", i);
+		usleep(5000);
+		pthread_mutex_unlock(&mutex);
+		pthread_mutex_lock(&mutex);
+		printf("Philosopher %d is sleeping\n", i);
+		usleep(5000);
+		pthread_mutex_unlock(&mutex);
+		pthread_mutex_lock(&mutex);
+		printf("Philosopher %d died\n", i);
+		usleep(5000);
+		pthread_mutex_unlock(&mutex);
+	}
 }
 
-static int	validate_args(t_philo *philo)
+int	main(void) 
 {
-	if (philo->argc != 6)
+	pthread_t	t1;
+	pthread_t	t2;
+	
+	pthread_mutex_init(&mutex, NULL);
+    if (pthread_create(&t1, NULL, (void *)routine, NULL) != 0)
 	{
-		write(2, "Usage: ./philosophers number_of_philosophers time_to_die time_to_eat time_to_sleep number_of_times_each_philosopher_must_eat\n", 125);
+		printf("Error creating thread 1");
 		return (1);
 	}
-	return (0);
-}
-
-//number of philosophers, time to die, time to eat, time to sleep, (number 
-//of times each philosopher must eat)
-int	main(int argc, char **argv)
-{
-	t_philo	philo;
-	size_t	i;
-	pthread_mutex_t	mutex;
-
-	i = 0;
-	(void)argv;
-	init_struct(&philo);
-	if (validate_args(&philo))
-		return (-1);
-	pthread_mutex_init(&mutex, NULL);
-	while (i <= philo.nbr)
-	{
-		pthread_create(pthread_t *restrict thread, const pthread_attr_t *restrict attr, void *(*start_routine)(void *), void *restrict arg);
-		i++;
+    if (pthread_create(&t2, NULL, (void *)routine, NULL) != 0)
+    {
+		printf("Error creating thread 2");
+		return (1);
+	}
+    if (pthread_join(t1, NULL) != 0)
+    {
+		printf("Error joining thread 1");
+		return (1);
+	}
+    if (pthread_join(t2, NULL) != 0)
+    {
+		printf("Error joining thread 2");
+		return (1);
 	}
 	pthread_mutex_destroy(&mutex);
-	return (0);
+	printf("Done\n");
+    return (0);
 }
