3f4ffa661344bc36face0f7056b8d0b705203e39
paularynty <paula.rynty@gmail.com>
2024-10-19 15:50:04 +0300

    validating args and initializing structs

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: prynty <prynty@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 14:38:32 by prynty            #+#    #+#             */
-/*   Updated: 2024/10/12 15:42:31 by prynty           ###   ########.fr       */
+/*   Updated: 2024/10/19 15:43:53 by prynty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,45 @@
 
 # include <stdlib.h> //for malloc, free
 # include <stdio.h> //for printf
+# include <string.h> //for memset
 # include <pthread.h> //for pthread functions
 # include <sys/time.h> //for gettimeofday
 # include <unistd.h> //for write, usleep
-# include <semaphore.h> //for sem_functions
 
-# define PHILOS 200
+# define PHILO_MAX 200
+# define RED "\033[1;91m"
+# define RESET "\033[0;39m"
 
 typedef struct s_philo
 {
-	int			argc;
-	char		**argv;
-	pthread_t	thread;
-	int			die;
-	int			eat;
-	int			sleep;
-	int			id; //individual philo id
-	size_t		nbr; //nbr of philosophers
+	int				argc;
+	char			**argv;
+	pthread_t		thread;
+	int				id; //individual philo id
+	int				num_of_philos;
+	int				eating;
+	int				meals_eaten;
+	size_t			last_meal;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	size_t			start_time;
+	size_t			num_times_to_eat;
+	int				*dead;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*write_lock;
+	pthread_mutex_t	*dead_lock;
+	pthread_mutex_t	*meal_lock;
 }	t_philo;
+
+typedef struct s_program
+{
+	int				dead_flag;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	meal_lock;
+	pthread_mutex_t	write_lock;
+	t_philo			*philos;
+}					t_program;
 
 #endif
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: prynty <prynty@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 14:38:20 by prynty            #+#    #+#             */
-/*   Updated: 2024/10/12 16:47:59 by prynty           ###   ########.fr       */
+/*   Updated: 2024/10/19 15:47:29 by prynty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,6 @@ TODO:
 4. Create a routine (what each philosopher has to do? in which order?)
 5. Initiate the threads with said routine
 */
-
-// void	init_struct(t_philo *philo)
-// {
-// 	philo->argc = argc;
-// 	philo->argv = argv;
-// 	philo->nbr = argv[1];
-// 	philo->die = argv[2];
-// 	philo->eat = argv[3];
-// 	philo->sleep = argv[4];
-// }
-
-// static int	validate_args(t_philo *philo)
-// {
-// 	if (philo->argc != 6)
-// 	{
-// 		write(2, "Usage: ./philosophers number_of_philosophers time_to_die time_to_eat time_to_sleep number_of_times_each_philosopher_must_eat\n", 125);
-// 		return (1);
-// 	}
-// 	return (0);
-// }
 
 // //number of philosophers, time to die, time to eat, time to sleep, (number 
 // //of times each philosopher must eat)
@@ -63,43 +43,134 @@ TODO:
 // 	return (0);
 // }
 
-pthread_mutex_t	mutex;
+static size_t	get_current_time(void)
+{
+	struct timeval	time;
 
+	if (gettimeofday(&time, NULL) == -1)
+		write(2, "gettimeofday failed\n", 21);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+static int	is_digit(char *str)
+{
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+static size_t	ft_atoi(char *str)
+{
+	long	result;
+	long	check;
+
+	result = 0;
+	check = result;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = (result * 10) + (*str - '0');
+		str++;
+	}
+	return ((size_t)(result));
+}
+
+void	print_error(char *msg)
+{
+	printf(RED"Error\n"RESET);
+	printf("%s\n", msg);
+	exit(EXIT_FAILURE);
+}
+
+void	validate_args(int argc, char **argv)
+{
+	int	i;
+
+	if (argc < 5 || argc > 6)
+		print_error("Usage: ./philo number_of_philosophers time_to_die time_to_eat time_to_sleep number_of_times_each_philosopher_must_eat");
+	i = 1;
+	while (i < argc)
+	{
+		if (!is_digit(argv[i]))
+			print_error("Arguments must be a positive number");
+		i++;
+	}
+}
+
+void	init_struct(t_philo *philos, int argc, char **argv)
+{
+	philos->argc = argc;
+	philos->argv = argv;
+	philos->num_of_philos = ft_atoi(argv[1]);
+	philos->time_to_die = ft_atoi(argv[2]);
+	philos->time_to_eat = ft_atoi(argv[3]);
+	philos->time_to_sleep = ft_atoi(argv[4]);
+	if (philos->argc == 6)
+		philos->num_times_to_eat = ft_atoi(argv[5]);
+	else
+		philos->num_times_to_eat = -1;
+	philos->start_time = get_current_time();
+	if (philos->num_of_philos > PHILO_MAX)
+		print_error("Too many philosophers");
+}
+
+static int	ft_usleep(size_t milliseconds)
+{
+	size_t	start;
+
+	start = get_current_time();
+	while ((get_current_time() - start) < milliseconds)
+		usleep(500);
+	return (0);
+}
+
+pthread_mutex_t	mutex;
 
 void	routine(void) 
 {
-	int i = 0;
+	int	i;
+	
+	i = 0;
 	while (i < 5)
 	{
 		i++;
 		pthread_mutex_lock(&mutex);
 		printf("Philosopher %d has taken a fork\n", i);
-		usleep(5000);
+		ft_usleep(500);
 		pthread_mutex_unlock(&mutex);
 		pthread_mutex_lock(&mutex);
 		printf("Philosopher %d is eating\n", i);
-		usleep(5000);
+		ft_usleep(500);
 		pthread_mutex_unlock(&mutex);
 		pthread_mutex_lock(&mutex);
 		printf("Philosopher %d is thinking\n", i);
-		usleep(5000);
+		ft_usleep(500);
 		pthread_mutex_unlock(&mutex);
 		pthread_mutex_lock(&mutex);
 		printf("Philosopher %d is sleeping\n", i);
-		usleep(5000);
+		ft_usleep(500);
 		pthread_mutex_unlock(&mutex);
 		pthread_mutex_lock(&mutex);
 		printf("Philosopher %d died\n", i);
-		usleep(5000);
+		ft_usleep(500);
 		pthread_mutex_unlock(&mutex);
 	}
 }
 
-int	main(void) 
+int	main(int argc, char **argv) 
 {
-	pthread_t	t1;
-	pthread_t	t2;
-	
+	pthread_t		t1;
+	pthread_t		t2;
+	t_philo			philos[PHILO_MAX];
+	pthread_mutex_t	forks[PHILO_MAX];
+	// t_program	program;
+
+	// memset(&philo, 0, sizeof(t_philo));
+	validate_args(argc, argv);
+	init_struct(philos, argc, argv);
 	pthread_mutex_init(&mutex, NULL);
     if (pthread_create(&t1, NULL, (void *)routine, NULL) != 0)
 	{
@@ -107,21 +178,21 @@ int	main(void)
 		return (1);
 	}
     if (pthread_create(&t2, NULL, (void *)routine, NULL) != 0)
-    {
+	{
 		printf("Error creating thread 2");
 		return (1);
 	}
-    if (pthread_join(t1, NULL) != 0)
-    {
+	if (pthread_join(t1, NULL) != 0)
+	{
 		printf("Error joining thread 1");
 		return (1);
 	}
     if (pthread_join(t2, NULL) != 0)
-    {
+	{
 		printf("Error joining thread 2");
 		return (1);
 	}
 	pthread_mutex_destroy(&mutex);
 	printf("Done\n");
-    return (0);
+	return (0);
 }
